c0e941b6825058ed42dbcf71f4b754acbe858697
Trần Võ Hoài Nguyên <nguyentvh0604@ut.edu.vn>
2026-01-09 13:13:35 +0700

    Add client source code

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+client.send("Hello Server".encode())
+client.close()
