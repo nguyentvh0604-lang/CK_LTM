09f7a3bf2f8d99324b2ea4c6661330dcd6eaf59a
Trần Võ Hoài Nguyên <nguyentvh0604@ut.edu.vn>
2026-01-11 21:26:51 +0700

    cap nhat client va server

--- client.py
+++ client.py
@@ -1,10 +1,55 @@
 import socket
+import threading
+import tkinter as tk
+from tkinter import scrolledtext, messagebox, simpledialog
 
 HOST = '127.0.0.1'
-PORT = 12345
+PORT = 5050
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+class ChatClient:
+    def __init__(self, host, port):
+        try:
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client.connect((host, port))
+        except ConnectionRefusedError:
+            print("Lỗi: Bạn chưa bật Server hoặc sai địa chỉ IP/Port!")
+            return
 
-client.send("Hello Server".encode())
-client.close()
+        self.root = tk.Tk()
+        self.root.title("Ứng dụng Chat")
+
+        self.nickname = simpledialog.askstring("Nickname", "Chọn tên hiển thị:", parent=self.root)
+        if not self.nickname: self.nickname = "Anonymous"
+
+        self.text_area = scrolledtext.ScrolledText(self.root)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.input_area = tk.Entry(self.root)
+        self.input_area.pack(padx=20, pady=5)
+        self.input_area.bind("<Return>", lambda e: self.write())
+
+        threading.Thread(target=self.receive, daemon=True).start()
+        self.root.mainloop()
+
+    def receive(self):
+        while True:
+            try:
+                message = self.client.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.client.send(self.nickname.encode('utf-8'))
+                else:
+                    self.text_area.config(state='normal')
+                    self.text_area.insert(tk.END, message + "\n")
+                    self.text_area.config(state='disabled')
+            except:
+                self.client.close()
+                break
+
+    def write(self):
+        msg = f"{self.nickname}: {self.input_area.get()}"
+        self.client.send(msg.encode('utf-8'))
+        self.input_area.delete(0, tk.END)
+
+if __name__ == "__main__":
+    ChatClient(HOST, PORT)
--- server.py
+++ server.py
@@ -1,18 +1,55 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
-PORT = 12345
+PORT = 5050
+ADDR = (HOST, PORT)
+FORMAT = 'utf-8'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen()
+server.bind(ADDR)
 
-print("Server đang chạy...")
+clients = []
+nicknames = []
 
-client, address = server.accept()
-print(f"Kết nối từ {address}")
+def broadcast(message):
+    """Gửi tin nhắn đến tất cả client đang kết nối"""
+    for client in clients:
+        client.send(message)
 
-message = client.recv(1024).decode()
-print("Client gửi:", message)
+def handle_client(client):
+    """Xử lý luồng dữ liệu riêng cho từng client"""
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} đã rời khỏi phòng chat!'.encode(FORMAT))
+            nicknames.remove(nickname)
+            break
 
-client.close()
+def receive():
+    """Chấp nhận kết nối mới liên tục"""
+    server.listen()
+    print(f"[SERVER] Đang chạy trên {HOST}...")
+    while True:
+        client, address = server.accept()
+        print(f"Kết nối mới từ {str(address)}")
+
+        client.send('NICK'.encode(FORMAT))
+        nickname = client.recv(1024).decode(FORMAT)
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname của client là {nickname}")
+        broadcast(f"{nickname} đã tham gia vào đoạn chat!".encode(FORMAT))
+        client.send('Đã kết nối đến server!'.encode(FORMAT))
+
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+receive()
